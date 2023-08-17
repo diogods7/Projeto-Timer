4ec1c811edfbdead82de0a553ddcebd1a99f3755
diogods7 <diogosalgueiro16@hotmail.com>
2023-08-17 14:48:55 -0300

    oi

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import './mode-theme.js'
+import * as FocusTimer from './FocusTimer/index.js'
+
+FocusTimer.start(12, 15)
--- js/mode-theme.js
+++ js/mode-theme.js
@@ -1,14 +1,10 @@
 const body = document.querySelector("body")
 const btnTheme = document.querySelector("#mode-theme")
-const iconDark = document.querySelector("#dark")
-const iconLight = document.querySelector("#light")
 
 
 function handleTheme() {
     body.classList.toggle("light-mode")
     body.classList.toggle("dark-mode")
-    iconDark.classList.toggle("hiden")
-    iconLight.classList.toggle("hiden")
     }
 
 btnTheme.addEventListener('click', handleTheme)
