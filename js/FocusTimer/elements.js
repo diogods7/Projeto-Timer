528a703b01999c46bcbc94f48130a7b0987cafdb
diogods7 <diogosalgueiro16@hotmail.com>
2023-09-11 14:43:02 -0300

    oi

--- js/FocusTimer/elements.js
+++ js/FocusTimer/elements.js
@@ -6,4 +6,10 @@ export const minutes = document.getElementById('minutes')
 
 export const seconds = document.getElementById('seconds')
 
-export const tree = document.getElementsByClassName("tree")
+export const tree = document.getElementById("button-tree")
+
+export const rain = document.getElementById("button-rain")
+
+export const coffee = document.getElementById("button-coffee")
+
+export const fire = document.getElementById("button-fire")
--- js/FocusTimer/music.js
+++ js/FocusTimer/music.js
@@ -1,20 +1,54 @@
 import state from './state.js'
+import * as sounds from './sound.js'
 import * as element from './elements.js'
 
 
 export function toggleTree (){
   state.isMute = document.documentElement.classList.toggle('music-on')
-  console.log(element.tree)
+  if(state.isMute){
+    sounds.buttonTree.play()
+    element.tree.classList.add("tree-focus")
+  }
+  else{
+    sounds.buttonTree.pause()
+    element.tree.classList.remove("tree-focus")
+  }
 }
 
 export function toggleRain (){
   state.isMute = document.documentElement.classList.toggle('music-on')
+  if(state.isMute){
+    sounds.buttonRain.play()
+    element.rain.classList.add("rain-focus")
+  }
+  else{
+    sounds.buttonRain.pause()
+    element.rain.classList.remove("rain-focus")
+  }
+
 }
 
 export function toggleCoffee (){
   state.isMute = document.documentElement.classList.toggle('music-on')
+  if(state.isMute){
+    sounds.buttonCoffee.play()
+    element.coffee.classList.add("coffee-focus")
+  }
+  else{
+    sounds.buttonCoffee.pause()
+    element.coffee.classList.remove("coffee-focus")
+  }
+
 }
 
 export function toggleFire (){
   state.isMute = document.documentElement.classList.toggle('music-on')
+  if(state.isMute){
+    sounds.buttonFire.play()
+    element.fire.classList.add("fire-focus")
+  }
+  else{
+    sounds.buttonFire.pause()
+    element.fire.classList.remove("fire-focus")
+  }
 }
